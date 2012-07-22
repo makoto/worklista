1b4ba5a0bff0519b7cc94abf43c14f73e47c3b69
Makoto Inoue <inouemak@googlemail.com>
2012-07-22 12:17:52 +0100

    Fixed failing cukes after upgrade

--- features/step_definitions/item_steps.rb
+++ features/step_definitions/item_steps.rb
@@ -1,16 +1,17 @@
 Given /^I am logged in as "([^"]*)"$/ do |arg1|
-  Given %{I am on the home page}
-  And   %{I follow "Sign up"}
-  And   %{I fill in "bob@example.com" for "user_email"}
-  And   %{I fill in "Bob" for "user_username"}
-  And   %{I fill in "testforbob" for "user_password"}
-  And   %{I fill in "testforbob" for "user_password_confirmation"}
-  And   %{I fill in "dummy_code" for "user_invite_code"}
-  And   %{I press "Sign up"}
-  And   %{I follow "Login"}
-  And   %{I fill in "bob@example.com" for "user_email"}
-  And   %{I fill in "testforbob" for "user_password"}
-  And   %{I press "Sign in"}
+  step %{I am on the home page}
+  step   %{I follow "Sign up"}
+  step   %{I fill in "bob@example.com" for "user_email"}
+  step   %{I fill in "Bob" for "user_username"}
+  step   %{I fill in "testforbob" for "user_password"}
+  step   %{I fill in "testforbob" for "user_password_confirmation"}
+  step   %{I fill in "dummy_code" for "user_invite_code"}
+  step   %{I press "Sign up"}
+  step   %{I follow "Logout"}
+  step   %{I follow "Login"}
+  step   %{I fill in "bob@example.com" for "user_email"}
+  step   %{I fill in "testforbob" for "user_password"}
+  step   %{I press "Sign in"}
 end
 
 Then /^I should have one item$/ do
